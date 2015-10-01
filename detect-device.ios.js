39486756e20fbc7bfc0f1cd4cc16c80641cda283
Isaac Madwed <i.madwed@gmail.com>
2015-10-01 09:53:53 -0400

    Redid files

--- detect-device.ios.js
+++ detect-device.ios.js
@@ -1,14 +1,3 @@
 import { UIDeviceHardware } from 'NativeModules';
 
-export default function makeDetectDevice (action) {
-  return function detectDevice() {
-    return function (dispatch) {
-      UIDeviceHardware.deviceInfo((info) => {
-        dispatch({
-          type: action,
-          info: info
-        });
-      });
-    };
-  };
-};
+export default UIDeviceHardware.deviceInfo;
